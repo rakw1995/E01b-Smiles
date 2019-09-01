c7d94c70085fbefd8e3bbb084f535fbe865d3b35
Gentry <watsonra@iu.edu>
2019-09-01 18:04:39 -0400

    finishes assignment and extra credit

--- README.md
+++ README.md
@@ -1,50 +1,17 @@
 # E01b-Smiles
-An exercise exploring variables and loops using Python Arcade.
+This code is composed of 5 parts, each intended to produce different variations of a "smile" image, and are as follows: 
+* Main1.py
+    In main1.py, a smile should be created by changing the location values of each shape (x,y) to produce a smile image.
+* Main2.py
+    In main2.py, the code establishes a name for the coordinates (face x, face y)
 
-This repository contains several files that you will need to alter to complete the assignment. The instructions for the exercise are also on Canvas.
+* Main3.py
+    In main3.py, the same smile should also be created, but this time it is centered as one complete object by defining the "center" and the location of other parts of the object in relation to that central point (face x, face y)
+* Main4.py
+    In main4.py, a repeating grid of the object should be created by assigning a loop to the values of x and y.
 
-Comments in Python are marked by a # sign (for single-line comments) or three matching quotation marks (''' or """) if a comment requires more than one line. They should also appear in a different color in VS Code. The Python Interpreter ignores comments, so you can safely include any information you want there.
-
-As with your assignments, please edit the LICENSE file (replace the [year] [fullname] with the current year and your name); you will also be expected to edit README.md (this file) to describe what you have accomplished.
-
----
-
-As always, Fork this repository, and then Clone it to your local computer.
-
-Before you begin, you will need to install Python Arcade, the graphics library we will be using for the first half of the semester. Open the Terminal (either by clicking on the magnifying glass and searching for CMD, or open the Terminal in VS Code) and type the following:
-
-*pip install arcade --user*
-
-pip is a tool for downloading and installing Python packages. arcade is the name of the package we will be using. When you type the command, you should see the components it is installing (with corresponding progress bars).
-
-When arcade has been installed, we can get to work:
-
-First, open main1.py. I am drawing several shapes that, if properly assembled, can form a smiley face. Right now, the shapes are all being drawn at (0,0), which is the bottom-left corner of the window. See if you can use those shapes to draw a smiley face in the center of the window. The small gray dots are catch lights for the eyes.
-
-The end result should look something like this:
-
-![Smile!](https://github.com/BL-MSCH-C220-F19/E01b-Smiles/blob/master/smile.png)
-
-When you have placed the shapes correctly, save the file and open main2.py. The only change I have made is to define coordinates (face_x and face_y) that represent the center of the circle. Figure out offsets for the other shapes in relation to face_x and face_y. Use face_x and face_y to move all the shapes to the middle of the window. Save your changes.
-
-Next, in main3.py, I have created several new coordinates to represent each of the shapes: smile, eye1, eye2, catch1, and catch2. Use what you learned in main2.py to define each of those coordinates in relation to face_x and face_y. Move the face to the middle of the window. You should only have to edit lines 13 through 18. Save your changes.
-
-Open main4.py. You will now be drawing many faces in the window. Editing lines 20 through 24, draw a grid of overlapping faces. Now, play with the values on lines 16 and 18. What happens when you adjust those numbers? Save your changes.
-
-When you have completed the exercise, commit your changes and push them back to GitHub. Turn in the URL of your repository on canvas.
-
-*Extra credit:* 
-
-main5.py is much more complicated, but it allows the smiley face to track mouse movements. Add a comment to every line describing what is happening. Save those changes.
-
-If you want to further explore Python Arcade, the API and some sample projects are available at [arcade.academy](http://arcade.academy).
-
----
-
-The grading criteria will be as follows:
-
-* [1 point] Repository contains an appropriate software license
-* [1 point] Repository contains a descriptive README.md
-* [8] Accomplishes the objective of the exercise
-
-10 points total
+* Main5.py
+In main5.py, the object should show up in the middle of the window until the mouse is moved, from which point it should follow the cursor movement by assigning values to three seperate functions:
+    1. __init__ will define what the code needs to do initially (how big the window will be, the window title, background color, and the mouse cursor)
+    2. on_draw will render the image according to the coordinates assigned
+    3. on_mouse_motion will tell the computer that whatever coordinates the mouse is within the window, so too should the values of the self object be
--- main5.py
+++ main5.py
@@ -1,54 +1,52 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 #this is the file location
 
-import utils, open_color, arcade
+import utils, open_color, arcade #this imports the resources needed for the code to work
 
-utils.check_version((3,7))
+utils.check_version((3,7)) #this verifies you are using the correct version of python
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Smiley Face Example"
+SCREEN_WIDTH = 800 #the window will be 800px in width
+SCREEN_HEIGHT = 600 #the window will be 600px in height
+SCREEN_TITLE = "Smiley Face Example" #the title in quotations will appear in the window's title line
 
-class Faces(arcade.Window):
-    """ Our custom Window Class"""
-
-    def __init__(self):
-        """ Initializer """
+class Faces(arcade.Window): #this defines the class
+    """ Our custom Window Class""" #this describes the previous line
+#this section within the class initializes the window and all of its values
+    def __init__(self): #this defines where all values attributed to self START.
+        """ Initializer """ #this describes the previous line
         # Call the parent class initializer
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE) #this prevents you from having to repeat the code for these values (it binds these values to the initializer)
 
-        # Show the mouse cursor
-        self.set_mouse_visible(True)
+        self.set_mouse_visible(True) # Show the mouse cursor when it is within the window
 
-        self.x = SCREEN_WIDTH / 2
-        self.y = SCREEN_HEIGHT / 2
+        self.x = SCREEN_WIDTH / 2 #this is where the object will start before the mouse moves, or "width" divided by 2
+        self.y = SCREEN_HEIGHT / 2 #this is where the object will start before the mouse moves, or "height" divided by 2
 
-        arcade.set_background_color(open_color.white)
+        arcade.set_background_color(open_color.white) #the background within the window will be white
+#this section renders the drawing
+    def on_draw(self): #this tells the computer "draw" the face
+        """ Draw the face """ #description of what the previous line should do
+        arcade.start_render() #this tag is required for the code to begin
+        face_x,face_y = (self.x,self.y) #this attributes the x and y values for the "face" to the "self" object(what is attributed to self, face will do)
+        smile_x,smile_y = (face_x + 0,face_y - 10) #the smile will be offset from the "face" by 0, -10.
+        eye1_x,eye1_y = (face_x - 30,face_y + 20) #eye1 will be offset by -30,20
+        eye2_x,eye2_y = (face_x + 30,face_y + 20) #eye2 will be offset by 30,20
+        catch1_x,catch1_y = (face_x - 25,face_y + 25) #catch1 will be offset by -25,25
+        catch2_x,catch2_y = (face_x + 35,face_y + 25) #catch2 will be offset by 35,25
 
-    def on_draw(self):
-        """ Draw the face """
-        arcade.start_render()
-        face_x,face_y = (self.x,self.y)
-        smile_x,smile_y = (face_x + 0,face_y - 10)
-        eye1_x,eye1_y = (face_x - 30,face_y + 20) 
-        eye2_x,eye2_y = (face_x + 30,face_y + 20)
-        catch1_x,catch1_y = (face_x - 25,face_y + 25) 
-        catch2_x,catch2_y = (face_x + 35,face_y + 25) 
+        arcade.draw_circle_filled(face_x, face_y, 100, open_color.yellow_3) #this creates a yellow circle that is 100px in radius
+        arcade.draw_circle_outline(face_x, face_y, 100, open_color.black,4) #this creates a black outline that is 100px in radius
+        arcade.draw_ellipse_filled(eye1_x,eye1_y,15,25,open_color.black) #this creates a black, filled ellipse that is 15x25px
+        arcade.draw_ellipse_filled(eye2_x,eye2_y,15,25,open_color.black)#this creates another black, filled ellipse that is 15x25px
+        arcade.draw_circle_filled(catch1_x,catch1_y,3,open_color.gray_2) #this creates a gray highlight that is 3px
+        arcade.draw_circle_filled(catch2_x,catch2_y,3,open_color.gray_2) #this creates another gray highlight that is 3px
+        arcade.draw_arc_outline(smile_x,smile_y,60,50,open_color.black,190,350,4) #this creates an outline in black that is used to create the smile (60widthx50height,190 is the starting degree of the angle, 350 is the ending degree of the angle. The line is 4px thick.)
 
-        arcade.draw_circle_filled(face_x, face_y, 100, open_color.yellow_3)
-        arcade.draw_circle_outline(face_x, face_y, 100, open_color.black,4)
-        arcade.draw_ellipse_filled(eye1_x,eye1_y,15,25,open_color.black)
-        arcade.draw_ellipse_filled(eye2_x,eye2_y,15,25,open_color.black)
-        arcade.draw_circle_filled(catch1_x,catch1_y,3,open_color.gray_2)
-        arcade.draw_circle_filled(catch2_x,catch2_y,3,open_color.gray_2)
-        arcade.draw_arc_outline(smile_x,smile_y,60,50,open_color.black,190,350,4)
-
-
-    def on_mouse_motion(self, x, y, dx, dy):
-        """ Handle Mouse Motion """
-        self.x = x
-        self.y = y
-
+#this section relates to mouse motion
+    def on_mouse_motion(self, x, y, dx, dy): #when the mouse moves, this tells the computer to move all objects that are attributed to what is in the parenthesis. 
+        """ Handle Mouse Motion """ #this explains to the user what the previous line is for
+        self.x = x #this attributes the self values of x to the mouse location in the window
+        self.y = y #this attributes the self values of y to the mouse location in the window
 
 
-window = Faces()
-arcade.run()
+window = Faces() #this is the ending for the Faces class and all values attributed to it
+arcade.run() #this runs the program until it is manually closed
